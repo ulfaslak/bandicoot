e1448fb74bb644860bc8878d10f3d7b44ea0f96b
ulfaslak <ulfjensen@gmail.com>
2017-02-02 14:42:58 +0100

    .

--- individual.py
+++ individual.py
@@ -132,7 +132,7 @@ def entropy(records, normalize=True):
     except AttributeError:
         counter = Counter(r.position for r in records)
 
-    print counter
+    print records[0]
     raw_entropy = entropy(counter.values())
     n = len(counter)
     if normalize and n > 1:
@@ -150,7 +150,7 @@ def interactions_per_contact(records):
         Filters the records by their direction: ``None`` for all records,
         ``'in'`` for incoming, and ``'out'`` for outgoing.
     """
-    
+
     try:
         contacts = [r.correspondent_id for r in records]
     except AttributeError:
@@ -161,7 +161,7 @@ def interactions_per_contact(records):
 @grouping(interaction=[['text', 'call'], 'physical'])
 def percent_ei_percent_interactions(records, percentage=0.8):
     """Percentage of contacts that account for 80%% of interactions.
-    
+
     If interaction is ['text', 'call'] normalize text interactions per correspondent
     with total number of texts and summed call duration per correspondent with total
     duration of calling, then combine those two measures and use that for user_count.
